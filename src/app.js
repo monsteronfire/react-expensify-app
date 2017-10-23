219f722eb30e3da58f8f444aa455ad13c8d48121
Shaii Ong <monsteronfire@gmail.com>
2017-10-23 22:51:27 +0800

    Add 404 page component

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route  } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path='/' exact component={ExpenseDashboardPage} exact={true} />
       <Route path='/create' component={AddExpensePage} />
       <Route path='/edit' component={EditExpensePage} />
       <Route path='/help' component={HelpPage} />
-    </div>
+      <Route component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 );
 
