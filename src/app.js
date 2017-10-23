dd6606728df6f403ddbbab276add2d198ecf37ae
Shaii Ong <monsteronfire@gmail.com>
2017-10-23 22:48:46 +0800

    Add main routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route  } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path='/' exact component={ExpenseDashboardPage} exact={true} />
+      <Route path='/create' component={AddExpensePage} />
+      <Route path='/edit' component={EditExpensePage} />
+      <Route path='/help' component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
