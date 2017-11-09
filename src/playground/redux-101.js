b6b9fc3c7f1fee69129489ec37f28f12e6e79c2a
Shaii Ong <monsteronfire@gmail.com>
2017-11-09 13:40:54 +0800

    Use switch instead of if-else

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,12 +1,13 @@
 import { createStore } from 'redux';
 
 const store = createStore((state = { count: 0 }, action) => {
-  if (action.type === 'INCREMENT') {
-    return {
-      count: state.count + 1
-    }
-  } else {
-    return state;
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    default:
+      return state;
   }
 });
 
@@ -14,3 +15,8 @@ console.log(store.getState());
 
 store.dispatch({type: 'INCREMENT'});
 
+console.log(store.getState());
+
+store.dispatch({type: 'INCREMENT'});
+
+console.log(store.getState());
